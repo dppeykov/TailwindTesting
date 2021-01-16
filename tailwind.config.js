440d0d1ac243105a49f8e30048564e2ee38d6e04
dpeykov <dpeykov@netsuite.com>
2021-01-16 15:19:37 +0100

    Testing different tailwind css features

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'Lato': ['Lato', 'sans-serif'],
+      'Cairo': ['Cairo', 'sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
